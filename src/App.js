1988f39c7f9b21f7bffe51649590b51c4a32e960
yasser-99 <yasser.alyobi@gmail.com>
2019-07-23 16:53:01 +0300

    updating the database and the server connections

--- src/App.js
+++ src/App.js
@@ -75,8 +75,9 @@ class App extends Component {
     this.setState({input:event.target.value});
   }
   onButtonSubmit = () => {
+    this.setState({boxes:[]});
     this.setState({url:this.state.input});
-    fetch('http://localhost:3000/imageurl',{
+    fetch('https://guarded-crag-34648.herokuapp.com/imageurl',{
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -85,7 +86,7 @@ class App extends Component {
     }).then(response => response.json())
       .then((response) => {
         if (response){
-          fetch('http://localhost:3000/image',{
+          fetch('https://guarded-crag-34648.herokuapp.com/image',{
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
